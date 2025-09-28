f1afeb1dbc602c36db09f6d170b424078e70ea34
mehorabkonok <konokmehorab@gmail.com>
2025-09-28 10:36:32 +0600

    'SVG' hamburger is  replaced by 'MENU'

--- src/Pages/Components/Navbar/Navbar.jsx
+++ src/Pages/Components/Navbar/Navbar.jsx
@@ -24,12 +24,13 @@ const Navbar = () => {
                 {/* Navbar start */}
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} className="btn btn-ghost lg:hidden border border-white h-10 w-24 px-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
+                        <div tabIndex={0} className="btn btn-ghost lg:hidden border border-white h-10 w-24 rounded-md px-4">
+                            {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
                                 viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                     d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
+                            </svg> */}
+                            MENU
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content bg-black/70 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                             {navItems}
